f9c68e81a269406918d3362eb95b2d5fc97e1366
Usman Babakolo <usmansbk@gmail.com>
2021-07-10 18:09:48 +0100

    Populate list with items

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const ITEMS = [
   {
     description: 'Wash the dishes',
-    completed: false,
+    completed: true,
     index: 0,
   },
   {
@@ -10,3 +10,31 @@ const ITEMS = [
     index: 1,
   },
 ];
+
+function listItemComponent({ id, description, completed }) {
+  const node = document.createElement('li');
+
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.checked = completed;
+  checkbox.id = id;
+
+  const label = document.createElement('label');
+  label.htmlFor = id;
+  label.innerText = description;
+
+  node.appendChild(checkbox);
+  node.appendChild(label);
+  return node;
+}
+
+function addItemsToDOM() {
+  const list = document.getElementById('items');
+  ITEMS.forEach((item) => {
+    list.appendChild(listItemComponent(item));
+  });
+}
+
+window.addEventListener('load', () => {
+  addItemsToDOM();
+});
