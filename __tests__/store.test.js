928d56efaba6e0a5dc4c079a76aaf389096df28a
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-07-21 08:39:19 -0400

    add Remove testing function

--- __tests__/store.test.js
+++ __tests__/store.test.js
@@ -1,4 +1,4 @@
-import createStore, { ADD_TODO } from '../src/store.js';
+import createStore, { ADD_TODO , DELETE_TODO } from '../src/store.js';
 
 describe('Adding an item to store', () => {
   test('should contain single item', () => {
@@ -25,3 +25,21 @@ describe('Adding an item to store', () => {
     expect(store.getState()).toHaveLength(0);
   });
 });
+
+test('should not have any elements', () => {
+  const store = createStore();
+      const action = {
+      type: ADD_TODO,
+      text: 'Testing with Jest',
+    };
+
+    store.dispatch(action);
+
+    const remove = {
+      type: DELETE_TODO,
+      index: 0,
+    };
+
+    store.dispatch(remove)
+    expect(store.getState()).toHaveLength(0);
+})
